2a70b872d221f3cb40b486ca0585cfdcc6b319a7
Ashley Rodriguez <arodriguez1030@gmail.com>
2020-04-20 00:35:03 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,7 +18,8 @@ def remove_strawberry(contacts)
 contacts.each  do |person, contact_details|
   if person == "Freddy Mercury"
     contact_details.each do |attribute, data|
-      binding.pry
+ if attribute == :favorite_ice_cream_flavors
+   data.delete_if {|ice_cream| ice_cream == "strawberry"}
     end
   end
 end
